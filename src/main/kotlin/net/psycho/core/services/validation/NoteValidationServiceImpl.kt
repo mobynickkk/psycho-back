0c2500cc32c8abbfb0f3511d2ec36bbcb5c8e4ba
mobynick <mobynick365@gmail.com>
2022-05-04 23:46:44 +0300

    fixed codestyle

--- src/main/kotlin/net/psycho/core/services/validation/NoteValidationServiceImpl.kt
+++ src/main/kotlin/net/psycho/core/services/validation/NoteValidationServiceImpl.kt
@@ -1,9 +1,10 @@
 package net.psycho.core.services.validation
 
+import org.springframework.stereotype.Service
+
 import net.psycho.core.domain.repositories.SchemaRepository
 import net.psycho.core.services.converters.schema.SchemaEntity2DtoConverter
 import net.psycho.core.services.dto.NoteDto
-import org.springframework.stereotype.Service
 
 @Service
 class NoteValidationServiceImpl(private val schemaRepository: SchemaRepository,
--- src/main/kotlin/net/psycho/core/ui/controller/NoteController.kt
+++ src/main/kotlin/net/psycho/core/ui/controller/NoteController.kt
@@ -1,10 +1,8 @@
 package net.psycho.core.ui.controller
 
-import org.springframework.data.repository.findByIdOrNull
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
